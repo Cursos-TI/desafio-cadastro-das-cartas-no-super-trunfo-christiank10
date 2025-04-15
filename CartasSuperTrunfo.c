7dd59cffad39019d023d7be917855f6c672905aa
christiank10 <christian.cl2017@icloud.com>
2025-04-15 01:13:58 +0000

    carta1 novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,6 +2,31 @@
 
 int main(){
     printf("cartas super trunfo!\n");
+    //// carta 1 ////
+    char estado[10] ="Ceara";
+    char codigo = 'C01';
+    char cidade[20] = "Fortaleza";
+    int populacao = 2574;
+    float area = 313;
+    float pib = 73;
+    int turisticos = 15;
+
+    printf("Estado: %s\n", estado);
+    printf("Codigo: %c\n", codigo);
+    printf("cidade: %s\n", cidade);
+    printf("Populacao: %i\n", populacao);
+    printf("Pib: %f\n", pib);
+    printf("Turisticos: %i\n", turisticos);
+
+     return 0;
+
+    
+    
+    
+    
+    
+    
+    
     printf("Novo commint\n");
 
 
