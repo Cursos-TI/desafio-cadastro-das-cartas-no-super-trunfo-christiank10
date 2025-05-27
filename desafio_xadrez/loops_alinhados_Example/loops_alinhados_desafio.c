75e5b3fede70793ac384fd6a7e41a90cc7897e15
christiank10 <christian.cl2017@icloud.com>
2025-05-27 00:03:52 +0000

    finalizadorecursivo

---
+++ desafio_xadrez/loops_alinhados_Example/loops_alinhados_desafio.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int cavalo = 1;
+
+
+    while (cavalo--)
+    {
+    for (int cavalo = 0; cavalo < 2; cavalo++) {
+        printf ("Cima\n");
+    }
+    printf ("Direita\n");
+
+    }
+
+     return 0;
+
+}
